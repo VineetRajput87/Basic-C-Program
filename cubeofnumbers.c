b808e5990b4011c749e131e4d5e5266c48601871
VineetRajput87 <114386532+VineetRajput87@users.noreply.github.com>
2022-09-29 21:47:46 +0530

    Add files via upload

---
+++ cubeofnumbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{  
+   int i,num;
+   printf("Input number of terms : ");
+   scanf("%d",&num);
+   for(i=1;i<=num;i++)
+   {
+       printf("\nNumber is : %d and cube of the %d is : %d",i,i,(i*i*i));
+   }
+   return 0;
+}
