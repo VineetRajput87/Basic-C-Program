e3ce8683788dc77b251814a80e4dda67015a8fea
VineetRajput87 <114386532+VineetRajput87@users.noreply.github.com>
2022-10-07 22:18:53 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int breath , length;
+    printf("Enter the lenth and breath : ");
+    scanf("%d %d",&length,&breath);
+    printf("The area of the Rectangle is : %d ",length*breath);
+    return 0;
+}
