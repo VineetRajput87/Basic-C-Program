3fcae0f1186ac70689cd2cf09b4ecd3985f49b33
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-21 20:28:56 +0530

    Add files via upload

---
+++ GreaterByIfWithArray2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter 4 Numbers : ");
+    scanf(" %d %d %d %d",&a,&b,&c,&d);
+    printf("\nThe Numbers are : %d %d %d %d ",a,b,c,d);
+    if(a>b)
+    {
+       if(a>c)
+       {
+            if(a>d)
+            {
+                printf("\nThe Greater Number Is : %d",a);   
+            }
+       } 
+    }   
+    else
+    {
+        if(b>c)
+        {
+            if (b>d)
+            {
+                printf("\nThe Greater Number Is : %d",b);
+            }
+            
+        }
+       else
+       {
+            if(c>d)
+            {
+                printf("\nThe Greater Number Is : %d",c);
+            }
+            else
+            {
+                printf("\nThe Greater Number Is : %d",d);
+            }
+        }
+    }
+    return 0;
+}
