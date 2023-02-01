2a01ebfdf1365645f694b84c1fd8d97800c274c1
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-01 20:28:03 +0530

    Create ReadPages.c

---
+++ ReadPages.c
@@ -0,0 +1,22 @@
+// Q.:- Vineet has started studying for the upcoming test. The textbook has N pages in total. Chef wants to read at most X pages a day for Y days.
+
+// Find out whether it is possible for Vineet to complete the whole book.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	    if((b*c)>=a)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+	return 0;
+}
+
