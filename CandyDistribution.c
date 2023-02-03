26862374175c2cb631b036423d3f08787b2a3762
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-03 19:19:22 +0530

    Create CandyDistribution.c

---
+++ CandyDistribution.c
@@ -0,0 +1,27 @@
+// Q.:- Vineet has N candies. He has to distribute them to exactly M of his friends such that each friend gets equal number of candies and each friend gets even number of candies. Determine whether it is possible to do so.
+
+// NOTE: Vineet will not take any candies himself and will distribute all the candies.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    if(a%b==0)
+	    {
+	        if((a/b)%2==0)
+	        printf("Yes\n");
+	        else
+	        printf("No\n");
+	    }   
+	    else
+	    printf("No\n");
+	}
+	return 0;
+}
+
