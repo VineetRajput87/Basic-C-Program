5c6504e116865821540496aa27547cad1cfea299
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-04 23:08:44 +0530

    Update PassTheExam.c

--- PassTheExam.c
+++ PassTheExam.c
@@ -1,10 +1,7 @@
 // Q.:- Vineet appeared for an exam consisting of 3 sections. Each section is worth 100 marks.
-
 // Vineet scored A marks in Section 1, 
 // B marks in section 2, and C marks in section 3.
-
 // Vineet passes the exam if both of the following conditions satisfy:
-
 // Total score of Chef is ≥100;
 // Score of each section ≥10.
 // Determine whether Chef passes the exam or not.
