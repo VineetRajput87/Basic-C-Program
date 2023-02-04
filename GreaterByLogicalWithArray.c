745c67e99ac5c18a994f22f94ac3f4ad1a1d046a
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-04 23:07:55 +0530

    Update GreaterByLogicalWithArray.c

--- GreaterByLogicalWithArray.c
+++ GreaterByLogicalWithArray.c
@@ -13,20 +13,12 @@ int main()
         printf("%d ",array[i]);
         }
     if(array[0]>array[1] && array[0]>array[2] && array[0]>array[3])
-    {
         printf("\nThe Greater Number Is : %d",array[0]);   
-    }   
     else if(array[1]>array[2] && array[1]>array[3])
-    {
         printf("\nThe Greater Number Is : %d",array[1]);
-    }
     else if(array[2]>array[3])
-    {
         printf("\nThe Greater Number Is : %d",array[2]);
-    }
     else
-    {
-        printf("\nThe Greater Number Is : %d",array[3]);
-    }   
+        printf("\nThe Greater Number Is : %d",array[3]);  
     return 0;
 }
