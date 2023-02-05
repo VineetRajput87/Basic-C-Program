f15a4c788536cbd81e37c0e5e76b121b70655b2b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-05 17:54:30 +0530

    Create XJumps.c

---
+++ XJumps.c
@@ -0,0 +1,23 @@
+// Q.:- Vineet is currently standing at stair 0 and he wants to reach stair numbered X.
+
+// Vineet can climb either Y steps or 1 step in one move.
+// Find the minimum number of moves required by him to reach exactly the stair numbered X.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    if(a%b==0)
+	    printf("%d\n",a/b);
+	    else
+	    printf("%d\n",(a/b)+(a%b));
+	}
+	return 0;
+}
+
