20fea2a4de2e20e0ca3f63cbdb670e96b3e34c5b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-06 13:53:39 +0530

    Create GoodProgram.c

---
+++ GoodProgram.c
@@ -0,0 +1,26 @@
+// Q.:- In computing, the collection of four bits is called a nibble.
+
+// Vineet defines a program as:
+
+// Good, if it takes exactly X nibbles of memory, where X is a positive integer.
+// Not Good, otherwise.
+// Given a program which takes N bits of memory, determine whether it is Good or Not Good.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    if(a%4==0)
+	    printf("Good\n");
+	    else
+	    printf("Not Good\n");
+	}
+	return 0;
+}
+
