cec626b05a4543679a9699da5713545d88ebc308
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-08 20:38:45 +0530

    Create BatteryHealth.c

---
+++ BatteryHealth.c
@@ -0,0 +1,22 @@
+// Q.:- Apple considers any iPhone with a battery health of 80% or above, to be in optimal condition.
+
+// Given that your iPhone has X% battery health, find whether it is in optimal condition.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    if(a>=80)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+	return 0;
+}
+
