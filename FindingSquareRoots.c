9afab96c631c6027ed59f62a9f3fc312a8b5f127
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-09 21:47:33 +0530

    Create FindingSquareRoots.c

---
+++ FindingSquareRoots.c
@@ -0,0 +1,24 @@
+// Q.:- In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
+
+// Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions. So here's your chance.
+
+//We have populated the solutions for the 10 easiest problems for your support.
+//Click on the SUBMIT button to make a submission to this problem.
+
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+	int t,n,r;
+	scanf("%d",&t);
+	while(t--)
+	{
+	scanf("%d",&n);  
+	r=sqrt(n);
+	printf("%d\n",r);
+	      
+	      
+	}
+	return 0;
+}
+
+
