dbb30842c2a263784fa4f41945ec670d5241437f
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-13 19:25:24 +0530

    Create NearestExit.c

---
+++ NearestExit.c
@@ -0,0 +1,28 @@
+// Q.:- There are two exits in a bus with 100 seats:
+
+// First exit is located beside seat number 1.
+// Second exit is located beside seat number 100.
+// Seats are arranged in a straight line from 1 to 100 with equal spacing between any 2 adjacent seats.
+
+// A passenger prefers to choose the nearest exit while leaving the bus.
+
+// Determine the exit taken by passenger sitting on seat X.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    if(a<=50)
+	    printf("LEFT\n");
+	    else
+	    printf("RIGHT\n");
+	}
+	return 0;
+}
+
