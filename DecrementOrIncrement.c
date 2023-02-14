1ab8e8a29fdd2d54ff921eb67d7a1afa23ed02b1
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-14 20:06:48 +0530

    Create DecrementOrIncrement.c

---
+++ DecrementOrIncrement.c
@@ -0,0 +1,15 @@
+// Q.:- Write a program to obtain a number N and increment its value by 1 if the number is divisible by 4ℎ otherwise decrement its value by 1.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	if(t%4==0)
+	printf("%d\n",t+1);
+	else
+	printf("%d\n",t-1);
+	return 0;
+}
+
