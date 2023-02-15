9568223b46e9a59abae20446ba59e5ec32c210e7
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-15 19:52:10 +0530

    Create VineetAndOperators.c

---
+++ VineetAndOperators.c
@@ -0,0 +1,24 @@
+// Q.:- Vineet has just started Programming, he is in first year of Engineering. Vineet is reading about Relational Operators.
+// Relational Operators are operators which check relationship between two values. 
+// Given two numerical values A and B you need to help Vineet in finding the relationship between them that is,First one is greater than second or, First one is less than second or, First and second one are equal.
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    if(a<b)
+	    printf("<\n");
+	    else if(a>b)
+	    printf(">\n");
+	    else
+	    printf("=\n");
+	}
+	return 0;
+}
+
