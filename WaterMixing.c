2752c58675dc412fa8415f9bc6b1bd9c3c8eb423
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-17 22:58:07 +0530

    Update WaterMixing.c

--- WaterMixing.c
+++ WaterMixing.c
@@ -1,6 +1,5 @@
 // Q.:- Vineet is setting up a perfect bath for himself. He has X litres of hot water and Y litres of cold water.
 // The initial temperature of water in his bathtub is A degrees. On mixing water, the temperature of the bathtub changes as following:
-
 // The temperature rises by 1 degree on mixing 1 litre of hot water.
 // The temperature drops by 1 degree on mixing 1 litre of cold water.
 // Determine whether he can set the temperature to B degrees for a perfect bath.
