9ae7f66f2f88f7d457daa0d5ef11a66dc8e00fc8
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-18 18:51:24 +0530

    Update BatteryHealth.c

--- BatteryHealth.c
+++ BatteryHealth.c
@@ -1,5 +1,4 @@
 // Q.:- Apple considers any iPhone with a battery health of 80% or above, to be in optimal condition.
-
 // Given that your iPhone has X% battery health, find whether it is in optimal condition.
 
 #include <stdio.h>
