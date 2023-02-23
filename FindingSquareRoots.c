5d4e85dc53b18dae6151db5e19db5a96ec4891be
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-23 22:38:20 +0530

    Update FindingSquareRoots.c

--- FindingSquareRoots.c
+++ FindingSquareRoots.c
@@ -1,6 +1,7 @@
 // Q.:- In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
 // Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions. So here's your chance.
 //We have populated the solutions for the 10 easiest problems for your support.
+
 //Click on the SUBMIT button to make a submission to this problem.
 
 #include <stdio.h>
