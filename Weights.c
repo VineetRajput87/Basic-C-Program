558d352dce8250d65af9faae9a5931cb8e2a9e04
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-04 20:03:46 +0530

    Update Weights.c

--- Weights.c
+++ Weights.c
@@ -1,5 +1,4 @@
 // Q.:- Vineet is playing with weights. He has an object weighing W units. He also has three weights each of X,Y, and Z units respectively. Help him determine whether he can measure the exact weight of the object with one or more of these weights.
-
 // If it is possible to measure the weight of object with one or more of these weights, print YES, otherwise print NO.
 
 #include <stdio.h>
