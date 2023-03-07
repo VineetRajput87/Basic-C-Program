8e086c776ac8281fe55c7ccf3d759c2160f6a997
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-07 10:41:19 +0530

    Update GoodProgram.c

--- GoodProgram.c
+++ GoodProgram.c
@@ -1,7 +1,6 @@
 // Q.:- In computing, the collection of four bits is called a nibble.
 
 // Vineet defines a program as:
-
 // Good, if it takes exactly X nibbles of memory, where X is a positive integer.
 // Not Good, otherwise.
 // Given a program which takes N bits of memory, determine whether it is Good or Not Good.
