068102eb4236c43f75e27f4f9bc08df737f779d2
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-12 19:21:20 +0530

    Update IntegerThatSumToTheSameValue.c

--- IntegerThatSumToTheSameValue.c
+++ IntegerThatSumToTheSameValue.c
@@ -1,5 +1,4 @@
 // Q.:- Alice has a positive integer N. She is wondering how many ordered pairs of positive integers (i,j) exist such that i + j = N.
-
 // Help Alice figure out the answer.
 
 // Note that since the pairs are ordered, (1,2) and (2,1) are considered different.
