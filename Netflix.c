9e00448ad1407af6ba34689a071bece1119a6a66
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-21 21:11:59 +0530

    Update Netflix.c

--- Netflix.c
+++ Netflix.c
@@ -1,5 +1,4 @@
 // Q.:- Alice, Bob, and Charlie are contributing to buy a Netflix subscription. However, Netfix allows only two users to share a subscription.
-
 // Given that Alice, Bob, and Charlie have A,B, and C rupees respectively and a Netflix subscription costs X rupees, find whether any two of them can contribute to buy a subscription.
 
 #include <stdio.h>
