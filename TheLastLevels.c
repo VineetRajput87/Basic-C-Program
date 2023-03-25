83daa257f4f8af982d87ed0197a67839ceb4e6db
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-25 23:22:23 +0530

    Update TheLastLevels.c

--- TheLastLevels.c
+++ TheLastLevels.c
@@ -1,7 +1,6 @@
 // Q.:- Vineet is playing a videogame, and is getting close to the end. He decides to finish the rest of the game in a single session.
 
 // There are X levels remaining in the game, and each level takes Vineet Y minutes to complete. To protect against eye strain, Vineet also decides that every time he completes 3 levels, he will take a Z minute break from playing. Note that there is no need to take this break if the game has been completed.
-
 // How much time (in minutes) will it take Chef to complete the game?
 
 #include <stdio.h>
