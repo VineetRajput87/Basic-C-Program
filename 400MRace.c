2aa6fc00963097d1e9bdbc3d54da1f603d22dd33
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-25 19:00:27 +0530

    Update 400MRace.c

--- 400MRace.c
+++ 400MRace.c
@@ -1,7 +1,6 @@
 // Q.:- Alice, Bob, and Charlie participated in a 400-metre race.
 
 // The time taken by Alice, Bob, and Charlie to complete the race was X,Y, and Z seconds respectively. Note that X,Y, and Z are distinct.
-
 // Determine the person having the highest average speed in the race.
 
 #include <stdio.h>
