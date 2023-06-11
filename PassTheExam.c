31142fee7d2d79c8898f82f38cb84965b091c9d5
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-11 18:55:13 +0530

    Update PassTheExam.c

--- PassTheExam.c
+++ PassTheExam.c
@@ -4,7 +4,7 @@
 // Vineet passes the exam if both of the following conditions satisfy:
 // Total score of Chef is ≥100;
 // Score of each section ≥10.
-// Determine whether Chef passes the exam or not.
+// Determine whether Chef passes the exam or not. 
 
 #include <stdio.h>
 int main(void) 
