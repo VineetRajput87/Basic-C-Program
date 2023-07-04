bc43633fab097ed4f92da1638d180c6e30666928
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-07-04 20:25:58 +0530

    Update XJumps.c

--- XJumps.c
+++ XJumps.c
@@ -1,7 +1,6 @@
 // Q.:- Vineet is currently standing at stair 0 and he wants to reach stair numbered X.
 // Vineet can climb either Y steps or 1 step in one move.
 // Find the minimum number of moves required by him to reach exactly the stair numbered X.
-
 #include <stdio.h>
 
 int main(void) 
